90942b1b336a6e3a33b59981ffc56cfce3a7353b
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-03-22 00:11:56 +0100

    updates

---
+++ src/context/AuthContext.js
@@ -0,0 +1,15 @@
+import React, { createContext } from 'react'
+
+
+
+const authContext  = createContext(null)
+
+
+const AuthContext = () => {
+
+  return (
+    <div>AuthContext</div>
+  )
+}
+
+export default AuthContext
