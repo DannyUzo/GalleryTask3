00e673d2e47db72bc5c23b53210c012b6cd0edb5
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-03-23 14:24:47 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom'
-import Home from './views/Home';
-import SignUp from './views/SignUp';
-import Login from './views/Login';
+import "./App.css";
+import Home from "./views/Home";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/signup' element={<SignUp/>}/>
-          <Route path='/login' element={<Login/>}/>
-      </Routes>
+      <Home />
     </div>
   );
 }
