f52948a7db583a4c274df1fd1f012d7b33df15df
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-04-26 16:16:26 +0100

    Updates: Footer File

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+import { Logo } from "./logo";
+
+export const Footer = () => {
+  return (
+    <footer className="flex-center">
+      <Logo />
+      <a href="https://github.com/DannyUzo">Github</a>
+    </footer>
+  );
+};
