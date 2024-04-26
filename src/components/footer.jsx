5a11615076f64a1641632d7b47b51be862efe0a0
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-04-26 16:48:15 +0100

    Updates: Footer file changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,11 @@
 import { Logo } from "./logo";
+import { GrGithub } from "react-icons/gr";
 
 export const Footer = () => {
   return (
     <footer className="flex-center">
       <Logo />
-      <a href="https://github.com/DannyUzo">Github</a>
+      <a href="https://github.com/DannyUzo"><GrGithub /></a>
     </footer>
   );
 };
